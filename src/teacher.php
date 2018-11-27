9faf19375371f5eb165f9ae2892689459fad28ec
k1_503_01 <k1_503_01@syktsu.ru>
2018-11-27 12:48:53 +0300

    Added teacher page (spectre)

---
+++ src/teacher.php
@@ -0,0 +1,82 @@
+<?php
+  require_once(realpath('./includes/page.php'));
+  require_once(realpath('./components/timeline.php'));
+  
+  $page = new Page();
+  $page->init('Main page');
+?>
+  
+  <div class="form-group">
+  <script>
+	function revertButtonHandler(element){
+		var selElement = element.parentNode.childNodes[3];
+		selElement.style.display='';
+		selElement.style.value='';
+	};
+  </script>
+  
+    <table  class="table table-bordered">
+      <thead>
+        <tr>
+                    <th scope="col">№</th>
+                    <th scope="col">Дата</th>
+                    <th scope="col">Время пары</th>
+                    <th scope="col">Группа</th>
+                    <th scope="col">Название пары</th>
+                    <th scope="col">Описание</th>
+                    <th scope="col">Проведена</th>
+                    <th scope="col">На доработку</th>          
+                    <th scope="col">Подпись</th>
+          
+         </tr>
+      </thead>
+      <tbody>
+           <tr>
+                    <th>1</th>
+                    <td>20.11.2018</td>
+                    <td>4 - 13:50-15:20</td>
+                    <td>147а</td>
+                    <td>Педагогика и Психология</td>
+                    <td>Личность человека и прочая лабуда</td>
+                    <td>Да</td>
+					<td class="text-center">
+                        <button class="btn btn-error" type="submit" onclick="revertButtonHandler(this)">Отправить</button>
+						<select style="display: none"> 
+							<option value="" disabled selected="">Причина отсутсвия</option> 
+							<option value="value1">Уважительная</option> 
+							<option value="value2">Неуважительная</option>
+						</select>
+                    </td>
+
+                    <td class="text-center">
+                        <button disabled class="btn btn-primary" type="submit">Подписать</button>
+                    </td>
+
+                </tr>
+                <tr>
+                    <th>2</th>
+                    <td>18.11.2018</td>
+                    <td>3 - 11:20-12:50</td>
+                    <td>147б</td>
+                    <td>ООП</td>
+                    <td>C++</td>
+                    <td>Нет</td>
+					<td class="text-center">
+                        <button class="btn btn-error" type="submit" onclick="revertButtonHandler(this)">Отправить</button>
+						<select style="display: none"> 
+							<option value="" disabled selected="">Причина отсутсвия</option> 
+							<option value="value1">Уважительная</option> 
+							<option value="value2">Неуважительная</option>
+						</select>
+                    </td>
+                    <td class="text-center">
+                        <button class="btn btn-primary center" type="submit">Подписать</button>
+                    </td>
+
+                </tr>
+          
+      </tbody>
+    </table>
+  </div>
+  </div>
+<?php $page->build(); ?>
