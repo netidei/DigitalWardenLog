f2ea31662fa5a2a0ebc56cd0131d893be82eff53
Roman <x0kill@ya.ru>
2018-11-30 17:06:27 +0300

    Update login page

--- src/pages/login.php
+++ src/pages/login.php
@@ -1,6 +1,5 @@
 <?php
     require_once realpath(__DIR__ . './page.php');
-    require_once realpath(__DIR__ . './form/form.php');
 
 class LoginPage extends Page
 {
@@ -8,32 +7,16 @@ class LoginPage extends Page
     protected function content($parameters)
     {
         $db = $this->getDatabase();
-        $form = new Form([
-            'method'=>'POST',
-            'content'=> function ($parameters) { ?>
-                <div class="form-group">
-                    <label class="form-label" for="loginInput">Login</label>
-                    <input class="form-input" type="text" name="login" id="loginInput" placeholder="Login">
-                </div>
-                <div class="form-group">
-                    <label class="form-label" for="passInput">Password</label>
-                    <input class="form-input" type="password" name="password" id="passInput">
-                </div>
-                <input class="btn btn-primary" type="submit" value="Log in">
-            <?php }
-        ]);
-        
-        $err = '';
+        $err = null;
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $username = self::POST('login');
             $password = self::POST('password');
             if ($username && $password) {
-                // Prepare a select statement
-                $user = new User($db, $username);
                 try {
+                    $user = new User($db, $username);
                     if ($user->validatePassword($password)) {
                         $user->startSession();
-                        header("location: index.php");
+                        header("Location: index.php");
                     }
                 } finally {
                     $err = "The login or password is invalid.";
@@ -42,10 +25,21 @@ class LoginPage extends Page
                 $err = 'Enter login and password';
             }
         }
-
         ?>
         <h1>Log in</h1>
-        <?php self::print($form);
-        echo($err);
+        <form method="POST">
+            <div class="form-group">
+                <label class="form-label" for="loginInput">Login</label>
+                <input class="form-input" type="text" name="login" id="loginInput" placeholder="Login">
+            </div>
+            <div class="form-group">
+                <label class="form-label" for="passInput">Password</label>
+                <input class="form-input" type="password" name="password" id="passInput">
+            </div>
+            <?php if ($err) { echo $err . '<br/>'; } ?>
+            <input class="btn btn-primary" type="submit" value="Log in">
+            
+        </form>
+        <?php
     }
 }
