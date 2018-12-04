59327b2e3ba9a80155d5ace37d18757009caec4c
Roman <x0kill@ya.ru>
2018-12-04 21:51:10 +0300

    Update router

--- src/components/router.php
+++ src/components/router.php
@@ -7,49 +7,80 @@ require_once realpath(__DIR__ . '/component.php');
 class Router extends Component
 {
 
-    private static function createPage($db, $user, $name)
-    {
-        $path = realpath(__DIR__ . '/pages/' . ($name ? $name :'main') . '.php');
-        if (file_exists($path)) {
-            return require_once $path;
-        }
-        return require_once realpath(__DIR__ . '/pages/404.php');
-    }
-
-    private static function Page($db, $user = null, $name = null)
-    {
-        $openPages = ['register', 'logout', '404'];
-        if ($user || in_array($name, $openPages)) {
-            switch ($name) {
-                case 'logout':
-                    $_SESSION = array();
-                    session_destroy();
-                    return self::Page($db);
-                default:
-                    return self::createPage($db, $user, $name);
-            }
-        }
-        return self::createPage($db, null, 'login');
-    }
-
     public function __construct()
     {
+        parent::__construct();
         $db = new DB();
         $user = null;
         session_start();
         if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
             $user = User::fromSession($db);
         }
-        parent::__construct();
         $this->define([
             'db'=>$db,
             'user'=>$user
         ]);
     }
 
-    public function render($props, $db, $user)
+    protected function render($props, $db, $user)
     {
         [$page] = self::extract($props, ['page'=>null]);
-        self::print(self::Page($db, $user, $page));
+        $data = $page ? $this->getPageByName($page) : $this->getPageByRole(3); // Default page
+        $name = $data[1];
+        $path = realpath(__DIR__ . "/pages/$name.php");
+        self::print(require_once $path);
+    }
+
+    private function hasAccess($data)
+    {
+        $role = $this->state['user'] ? $this->state['user']->getRole() : 4;
+        if ($role == '0') {
+            return true;
+        }
+        switch ($data[3]) {
+            case '0':
+                return true;
+            default:
+                if ($role == '4') {
+                    return false;
+                }
+                $list = $this->getAccessList($data[0]);
+                $inList = in_array($role, $list) || (count($list) > 0 && $list[0] == '0');
+                return $data[3] == '1' ? $inList : !$inList;
+        }
+    }
+
+    private function getPage($where)
+    {
+        $pageData = $this->state['db']->select('page', '*', $where);
+        if ($pageData->count() > 0) {
+            $data = $pageData->row();
+            if ($this->hasAccess($data)) {
+                return $data;
+            } else {
+                return $this->getPageByRole(1); // Login page
+            }
+        }
+        return $this->getPageByRole(0); // 404 page
+    }
+
+    private function getPageByRole($role)
+    {
+        return $this->getPage('`role` = ' . $role);
+    }
+
+    private function getPageByName($name)
+    {
+        return $this->getPage('`name` = "' . $name . '"');
+    }
+
+    private function getAccessList($id)
+    {
+        $data = array();
+        $list = $this->state['db']->select('access_list', '`role`', '`page` = ' . $id);
+        while ($row = $list->row()) {
+            array_push($data, $row[0]);
+        }
+        return $data;
     }
 }
